9ea8d3790c147fa748066099014474d5331db249
Spencer Schaus <assist.totheregionalmanager@gmail.com>
2020-05-04 22:26:00 -0400

    Created quote array in script.js. The quote array contains 5 objects that each hold the following keys: quote, source, citation, and year.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,6 +11,38 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+quotes = [
+    {
+        quote: '"I love inside jokes. I hope to be a part of one someday."',
+        source: 'Michael Scott',
+        citation: 'The Office',
+        year: '2006'
+    },
+    {
+        quote: '"You couldn\'t handle my undivided attention."',
+        source: 'Dwight Schrute',
+        citation: 'The Office',
+        year: '2010'
+    },
+    {
+        quote: '"Andy Bernard does not lose contests. He wins them. Or he quits them. Because they\'re unfair."',
+        source: 'Andy Bernard',
+        citation: 'The Office',
+        year: '2009'
+    },
+    {
+        quote: '"This is Pretzel Day"',
+        source: 'Stanley Hudson',
+        citation: 'The Office',
+        year: '2006'
+    },
+    {
+        quote: '"I got six numbers. One more and it would have been a complete phone number"',
+        source: 'Kevin Malone',
+        citation: 'The Office',
+        year: '2009'
+    },
+]
 
 
 /***
