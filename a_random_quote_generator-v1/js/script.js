aa0b0ab21354a17d59d836c2d88cfb7c959ec883
Spencer Schaus <assist.totheregionalmanager@gmail.com>
2020-05-13 20:34:15 -0400

    - Added Colors array - moved randomQuote variable into the printQuote function (This fixed the issue where click the get quote button generated a random quote each time.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -7,7 +7,41 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
+/***
+ * Colors for changing the background
+***/
+
+// background: linear-gradient(to right, #43cea2, #185a9d)
+// background: linear-gradient(to right, #ff416c, #ff4b2b);
+// background: linear-gradient(to right, #396afc, #2948ff);
+// background: linear-gradient(to right, #3494e6, #ec6ead);
+// background: linear-gradient(to right, #8e0e00, #1f1c18);
+
+//Body Colors Array:
+
+colors = [
+    {
+        bcolor: "red"
+    },
+    {
+        bcolor: "green"
+    },
+    {
+        bcolor: "blue"
+    },
+    {
+        bcolor: "purple"
+    },
+    {
+        bcolor: "yellow"
+    }
+
+
+];
+
+//print color to body code: document.body.style.backgroundColor = "red";
+
+/***
  * `quotes` array 
 ***/
 
@@ -42,7 +76,7 @@ quotes = [
         citation: 'The Office',
         year: '2009'
     },
-]
+];
 
 
 /***
@@ -53,25 +87,28 @@ quotes = [
 
 function getRandomQuote(quoteParam) {
     let randomizer = quoteParam[Math.floor(Math.random() * quoteParam.length)];
-    return randomizer;
+    let colorRandomizer = colorParam[Math.floor(Math.random() * colorParam.length)];
+    return [randomizer, colorRandomizer];
  }
 
-const randomQuote =  getRandomQuote(quotes);
+
 
 /***
  * `printQuote` function
 ***/
 
 
-function printQuote(printParam){
-    HTMLbuild = `<p class="quote"> ${printParam.quote}</p><p class="source">${printParam.source}<span class="citation">${printParam.citation}</span><span class="year">${printParam.year}</span></p>`;
+function printQuote(){
+    const randomQuote =  getRandomQuote(quotes, colors);
+    HTMLbuild = `<p class="quote"> ${randomQuote.quote}</p><p class="source">${randomQuote.source}<span class="citation">${randomQuote.citation}</span><span class="year">${randomQuote.year}</span></p>`;
+    let randomColor = randomQuote.bcolor;
 
-    return document.getElementById('quote-box').innerHTML = HTMLbuild;
+    return [document.getElementById('quote-box').innerHTML = HTMLbuild, document.body.style.backgroundColor = randomColor]
 
-    // console.log(HTMLbuild);
+
 }
 
- printQuote(randomQuote);
+ printQuote();
 
 
 /***
